a0e055529b3bb513f4e4d18835f66acf7c482ade
unman <unman@thirdeyesecurity.org>
2025-12-21 12:16:27 +0000

    Make description scrollable.

--- qubes_task_gui.py
+++ qubes_task_gui.py
@@ -52,11 +52,24 @@ class QubesPackageManager(QMainWindow):
         self.package_table.horizontalHeader().setStretchLastSection(True)
         self.package_table.setHorizontalHeaderLabels(['Package', 'Status', 'Description'])
         self.package_table.verticalHeader().setVisible(False)
+        self.package_table.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
         self.package_table.setColumnWidth(0, 300)
         self.package_table.setColumnWidth(1, 100)
         main_layout.addWidget(self.package_table)
 
+        self.package_table.cellClicked.connect(self.show_package_info)
         QTimer.singleShot(10, self.populate_package_list)
+
+    def init_description_column(self, pkg_count: int):
+        placeholder = QTableWidgetItem("")
+        placeholder.setFlags(placeholder.flags() & ~Qt.ItemFlag.ItemIsEditable)
+        placeholder.setTextAlignment(
+            Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft
+        )
+        self.package_table.setItem(0, 2, placeholder)
+
+        if pkg_count > 1:
+            self.package_table.setSpan(0, 2, pkg_count,  1)
 
     def populate_package_list(self):
         self.progress_dialog = QProgressDialog("Fetching Task List", "Cancel", 0, 0, self)
@@ -76,7 +89,8 @@ class QubesPackageManager(QMainWindow):
         self.worker_thread.start()
 
     def update_package_list(self, packages):
-        self.package_table.setRowCount(len(packages))
+        pkg_count = len(packages)
+        self.package_table.setRowCount(pkg_count)
     
         status = "Installed"
         for row, package in enumerate(packages):
@@ -89,19 +103,21 @@ class QubesPackageManager(QMainWindow):
                     status = "Available"
                     status_item = QTableWidgetItem(status)
                 package_item = QTableWidgetItem(trimmed_package)
-                if 'packages' in trimmed_package :
-                    package_item.setFlags(package_item.flags() & ~Qt.ItemFlag.ItemIsEditable & ~Qt.ItemFlag.ItemIsSelectable) 
-                else:
+                if '3isec' in trimmed_package :
                     package_item.setFlags(package_item.flags() & ~Qt.ItemFlag.ItemIsEditable)
+                else:
+                    package_item.setFlags(package_item.flags() & ~Qt.ItemFlag.ItemIsEditable & ~Qt.ItemFlag.ItemIsSelectable) 
                 self.package_table.setItem(row, 0, package_item)
 
                 status_item = QTableWidgetItem(status)
                 status_item.setFlags(status_item.flags() & ~Qt.ItemFlag.ItemIsEditable & ~Qt.ItemFlag.ItemIsSelectable) 
                 self.package_table.setItem(row, 1, status_item)
+
+        self.init_description_column(pkg_count)
     
         self.statusBar().showMessage(f"Retrieved {len(packages)-2} packages", 5000)
 
-        description_item = QTableWidgetItem("Description test")
+        description_item = QTableWidgetItem("")
         description_item.setFlags(package_item.flags() & ~Qt.ItemFlag.ItemIsEditable & ~Qt.ItemFlag.ItemIsSelectable) 
 
     def cancel_and_quit(self):
@@ -111,6 +127,36 @@ class QubesPackageManager(QMainWindow):
             self.worker_thread.terminate()
             self.worker_thread.wait()
         QApplication.instance().quit()
+
+    def task_info(self, package):
+        try:
+            cmd = ['rpm','-qi',package]
+            result = subprocess.run(cmd, capture_output=True, text=True)
+            if result.returncode == 0:
+                lines = result.stdout.split('\n')
+                for line in lines:
+                    if line.startswith('Summary'):
+                        summary = line.split(':', 1)[1].strip()
+                    elif line.startswith('Description'):
+                        description_lines = []
+                        for description_line in lines[lines.index(line)+1:]:
+                            description_lines.append(description_line.strip())
+                        description = '\n '.join(description_lines)
+                return f"Summary: {summary}\nDescription: {description}"
+            else:
+                return f"No information"
+        except Exception as e:
+            return f"Failed to get information: {e}"
+
+    def show_package_info(self, row, column):
+        if column == 0:
+            if self.package_table.item(row, 0) is not None:
+                selected_package = self.package_table.item(row, 0).text()
+                description = self.task_info(selected_package).strip()
+            else:
+                description = ""
+            description_item = QTableWidgetItem(description)
+            self.package_table.setItem(0, 2, description_item)
 
     def handle_error(self, message):
         self.statusBar().showMessage(message, 5000)
